3043dbabc998c4822df825433ec2f0e9b5f20256
Jonathan <jclark81@student.gsu.edu>
2019-03-08 15:48:13 -0500

    A working landing page that sends data to mongodb. On submit button click, it takes you to rating page (doesn't exist yet). Styling fixed and connection fixed.

--- app.js
+++ app.js
@@ -3,29 +3,36 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require("mongoose");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+
+//server logging
 app.use(logger('dev'));
+
+//body-parser
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+//use the static directory name + public folder which has css in stylesheets folder
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -37,5 +44,42 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+mongoose.Promise = global.Promise;
+//connecting to local db
+mongoose.connect("mongodb://localhost:27017/node-demo1");
+
+
+//schema
+var nameSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email:String,
+    phone:String,
+    age:String
+});
+
+//variable 'User' to reference later
+var User = mongoose.model("User", nameSchema);
+
+
+//post that data to the database
+app.post("/test-page", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+
+                res.redirect('/users');
+
+        })
+        .catch(err => {
+            res.status(400).send("unable to save to database");
+        });
+});
+
+
+
+
 
 module.exports = app;
